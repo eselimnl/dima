85cc7040708547a562b1b51cbc39627ca21c0cd9
Eyyüb Selim <63191434+eselimnl@users.noreply.github.com>
2022-03-29 11:09:32 +0300

    remove emoji from conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ release = '..'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser', 'sphinxemoji.sphinxemoji']
+extensions = ['myst_parser']
 myst_enable_extensions = [
     "deflist"
 ]
